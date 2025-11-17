06fbf7cfaac4568f60f930b292a6a47a5591b800
Christoffer Gylin <christoffer@christoffergylin.com>
2025-11-17 17:08:31 +0100

    Created i18n config file

---
+++ src/i18next/i18n.ts
@@ -0,0 +1,18 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector'
+
+i18n.use(I18nextBrowserLanguageDetector).use(initReactI18next).init({
+    debug:true,
+    fallbackLng: 'en',
+    interpolation: {
+        escapeValue: false,
+    },
+    resources: {
+        en: {
+            translation: {}
+        }
+    }
+})
+
+export default i18n
