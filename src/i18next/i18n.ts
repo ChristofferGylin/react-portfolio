b1858f1dd2f4d3407d288f40827b188e54ec4b03
Christoffer Gylin <christoffer@christoffergylin.com>
2025-11-17 20:12:33 +0100

    Installed i18next-http-backend and implemented in i18n config file

--- src/i18next/i18n.ts
+++ src/i18next/i18n.ts
@@ -1,31 +1,14 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector'
+import I18NextHttpBackend from 'i18next-http-backend'
 
-i18n.use(I18nextBrowserLanguageDetector).use(initReactI18next).init({
+i18n.use(I18NextHttpBackend).use(I18nextBrowserLanguageDetector).use(initReactI18next).init({
     debug:true,
     fallbackLng: 'en',
     interpolation: {
         escapeValue: false,
     },
-    resources: {
-        en: {
-            translation: {
-                welcome: {
-                    part1: 'Welcome <1>beautiful</1> people.',
-                    part2: 'This is my portfolio',
-                }
-            }
-        },
-        sv: {
-            translation: {
-                welcome: {
-                    part1: 'Välkommen <1>vackra</1> människor.',
-                    part2: 'Det här är min portfolio',
-                }
-            }
-        }
-    }
 })
 
 export default i18n
