87af304921460aa07242d6419007cebebec63ed7
Christoffer Gylin <christoffer@christoffergylin.com>
2025-11-17 18:47:09 +0100

    Created temporary language switcher in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import { Trans, useTranslation } from "react-i18next"
 
+const lngs = {
+  en: { nativeName: 'English'},
+  sv: { nativeName: 'Svenska'},
+}
+
 function App() {
 
-  const { t } = useTranslation()
+  const { t, i18n } = useTranslation()
 
   return (
     <>
+    <div className="flex gap-2">
+      {(Object.keys(lngs) as Array<keyof typeof lngs>).map((lng) => {
+        return (
+          <button key={lng} type="submit" onClick={() => i18n.changeLanguage(lng)} className="border rounded bg-blue-200 p-1">
+            {lngs[lng].nativeName}
+          </button>
+        )
+      })}
+    </div>
       <h1 className="text-3xl text-purple-500">Vite + React = Portfolio</h1>
       <Trans i18nKey={'welcome.part1'}>
         Welcome <span className="text-pink-600 text-xl animate-bounce">beautiful</span> people
