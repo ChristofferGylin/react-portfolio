a8cb3afedb6a17187e4ef04d1b5d48899fc20b1b
Christoffer Gylin <christoffer@christoffergylin.com>
2025-11-25 18:52:10 +0100

    Copied DesktopMenu component from next-portfolio

---
+++ src/components/menu/DesktopMenu.tsx
@@ -0,0 +1,21 @@
+
+import LanguageSelector from "./LanguageSelector"
+import MenuItem from "./MenuItem"
+
+const DesktopMenu = () => {
+
+    return (
+        <nav className="hidden md:flex gap-3 h-full">
+                <ul className="flex justify-between">
+                    <MenuItem href="/" title={"Home"} />
+                    <MenuItem href="/about" title={"About"} />
+                    <MenuItem href="/portfolio" title={"Portfolio"} />
+                    <MenuItem href="/cv" title={"CV"} />
+                    <MenuItem href="/contact" title={"Contact"} />
+                </ul>
+                <LanguageSelector />
+        </nav>
+    )
+}
+
+export default DesktopMenu
