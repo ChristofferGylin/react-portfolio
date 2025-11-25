4fda7db66b36836accd8d5b1a37cf5bb1238f26e
Christoffer Gylin <christoffer@christoffergylin.com>
2025-11-25 19:53:58 +0100

    Implemented translations from i18n in DesktopMenu component

--- src/components/menu/DesktopMenu.tsx
+++ src/components/menu/DesktopMenu.tsx
@@ -1,16 +1,19 @@
+import { useTranslation } from "react-i18next"
 import LanguageSelector from "./LanguageSelector"
 import MenuItem from "./MenuItem"
 
 const DesktopMenu = () => {
 
+    const { t } = useTranslation(['menu'])
+
     return (
         <nav className="hidden md:flex gap-3 h-full">
                 <ul className="flex justify-between">
-                    <MenuItem href="/" title={"Home"} />
-                    <MenuItem href="/about" title={"About"} />
-                    <MenuItem href="/portfolio" title={"Portfolio"} />
-                    <MenuItem href="/cv" title={"CV"} />
-                    <MenuItem href="/contact" title={"Contact"} />
+                    <MenuItem href="/" title={t('home', { ns: 'menu'})} />
+                    <MenuItem href="/about" title={t('about', { ns: 'menu'})} />
+                    <MenuItem href="/portfolio" title={t('portfolio', { ns: 'menu'})} />
+                    <MenuItem href="/cv" title={t('cv', { ns: 'menu'})} />
+                    <MenuItem href="/contact" title={t('contact', { ns: 'menu'})} />
                 </ul>
                 <LanguageSelector />
         </nav>
