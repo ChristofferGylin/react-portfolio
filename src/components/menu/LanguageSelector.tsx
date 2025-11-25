aed577f9f738cdbe8e2e52b2da4777b66b02c454
Christoffer Gylin <christoffer@christoffergylin.com>
2025-11-25 19:33:59 +0100

    Updated onSetLanguage fn in LanguageSelector to chaneg i18n language

--- src/components/menu/LanguageSelector.tsx
+++ src/components/menu/LanguageSelector.tsx
@@ -1,27 +1,26 @@
+import { useTranslation } from "react-i18next"
 import ImageButton from "../UI/ImageButton"
 import Indicator from "../UI/Indicator"
 
 const LanguageSelector = () => {
 
-    //const languageIndicatorPosition = language === 'en' ? 'translate-x-[34px]' : ''
+    const { i18n } = useTranslation()
 
-    // const onSetLanguage = (newLang: AvailibleLanguages) => {
-    //     languageSetter(newLang)
-        
-    //     if (cookieConsent) {
-    //         setLanguageCookie(newLang)
-    //     }
-    // }
+    const languageIndicatorPosition = i18n.resolvedLanguage === 'en' ? 'translate-x-[34px]' : ''
+
+    const onSetLanguage = (newLang: 'en' | 'se') => {
+        i18n.changeLanguage(newLang)
+    }
 
     return (
         <div className="flex flex-col justify-center items-center gap-0.5 h-full pt-1.5">
             <div className="flex justify-between w-[54px] h-5">
-                <ImageButton src={"/flags/se.svg"} alt='Change language to Swedish' callback={() => {}} />
+                <ImageButton src={"/flags/se.svg"} alt='Change language to Swedish' callback={() => {onSetLanguage('se')}} />
                 <p className="flex justify-center items-center">/</p>
-                <ImageButton src={"/flags/gb.svg"} alt='Change language to English' callback={() => {}} />
+                <ImageButton src={"/flags/gb.svg"} alt='Change language to English' callback={() => {onSetLanguage('en')}} />
             </div>
             <div className={`flex w-[60px]`}>
-                <Indicator width="w-[26px]" style={""} />
+                <Indicator width="w-[26px]" style={languageIndicatorPosition} />
             </div>
         </div>
     )
