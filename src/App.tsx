d57b6f18d6ef3ecf4aad0963b709eedb0741256b
Christoffer Gylin <christoffer@christoffergylin.com>
2025-11-25 19:59:13 +0100

    Adde flag images to public and implemented Menu component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from "react"
 import { useTranslation } from "react-i18next"
+import Menu from "./components/menu/Menu"
 
 const lngs = {
   en: { nativeName: 'English'},
@@ -8,27 +9,11 @@ const lngs = {
 
 function App() {
 
-  const { t, i18n } = useTranslation(['home'])
+  // const { t, i18n } = useTranslation(['home'])
 
   return (
     <>
-    <div className="flex gap-2">
-      {(Object.keys(lngs) as Array<keyof typeof lngs>).map((lng) => {
-        return (
-          <button key={lng} type="submit" onClick={() => i18n.changeLanguage(lng)} className="border rounded bg-blue-200 p-1">
-            {lngs[lng].nativeName}
-          </button>
-        )
-      })}
-    </div>
-      <p>{t('intro.hi', { ns: 'home'})}</p>
-      <h1 className="text-3xl text-purple-500">Christoffer Gylin</h1>
-      <p>
-        {t('intro.tagline', {ns: 'home'})}
-      </p>
-      <p>
-        {t('intro.text', {ns: 'home'})}
-      </p>
+      <Menu />
     </>
   )
 }
