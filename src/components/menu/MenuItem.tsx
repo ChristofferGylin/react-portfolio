44bdac179156c70beb677f38f9a37bf5da359d0e
Christoffer Gylin <christoffer@christoffergylin.com>
2025-11-26 21:43:21 +0100

    Changed MenuItem to use Link from React Router for navigation

--- src/components/menu/MenuItem.tsx
+++ src/components/menu/MenuItem.tsx
@@ -1,4 +1,5 @@
 import { type ReactElement } from "react";
+import { Link } from "react-router-dom";
 
 type MenuItemProps = {
     href: string;
@@ -14,10 +15,10 @@ const MenuItem = ({ href, title, Icon }: MenuItemProps) => {
     return (
         <li className="hover:bg-white/5 md:hover:bg-white/0 transition-all duration-150 md:border-0 flex md:items-center md:justify-center p-2 border-b last:border-0 border-slate-400/10 h-full w-48 md:w-fit">
             <div className={`${highlighted ? 'underline underline-offset-4' : ''} hover:text-sky-50 text-lg transition-all duration-150 ease-in-out w-full`}>
-                <a href={href} className="flex gap-2 items-center w-full">
-                     {Icon && Icon}
+                <Link to={href}>
+                    {Icon && Icon}
                     {title}
-                </a>
+                </Link>
             </div>
         </li>
     )
