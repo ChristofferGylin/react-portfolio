0f9dc7abaea0dc533a086e67ab385b7fc4b0a734
Christoffer Gylin <christoffer@christoffergylin.com>
2025-11-26 21:39:56 +0100

    Created routes in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import { Suspense } from "react"
 // import { useTranslation } from "react-i18next"
 import Menu from "./components/menu/Menu"
+import { Route, Routes } from "react-router-dom"
+import Home from "./components/home/Home"
+import About from "./components/about/About"
+import Contact from "./components/contact/Contact"
+import CV from "./components/cv/CV"
+import Portfolio from "./components/portfolio/Portfolio"
+import NotFound from "./components/notFound/Not Found"
 
 function App() {
 
@@ -9,6 +16,14 @@ function App() {
   return (
     <>
       <Menu />
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cv" element={<CV />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   )
 }
