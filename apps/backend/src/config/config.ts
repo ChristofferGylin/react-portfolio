4b949bdee0411f7c9e965577cc18e249a0826eb2
Christoffer Gylin <christoffer@christoffergylin.com>
2025-11-28 20:30:32 +0100

    Added github token to env config

--- apps/backend/src/config/config.ts
+++ apps/backend/src/config/config.ts
@@ -5,11 +5,13 @@ configDotenv()
 type Config = {
     port: number;
     nodeEnv: string;
+    githubToken: string;
 }
 
 const config: Config = {
     port: Number(process.env.PORT) || 3000,
     nodeEnv: process.env.NODE_ENV || "development",
+    githubToken: process.env.GITHUB_TOKEN || ''
 }
 
 export default config
