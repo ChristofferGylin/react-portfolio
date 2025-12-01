9de6ce8e974fec9023d9544ac88550f99098645a
Christoffer Gylin <christoffer@christoffergylin.com>
2025-12-01 16:24:56 +0100

    Added email credentials to dotenv config

--- apps/backend/src/config/config.ts
+++ apps/backend/src/config/config.ts
@@ -6,12 +6,18 @@ type Config = {
     port: number;
     nodeEnv: string;
     githubToken: string;
+    smtpUrl: string;
+    emailUser: string;
+    emailPassword: string;
 }
 
 const config: Config = {
     port: Number(process.env.PORT) || 3000,
     nodeEnv: process.env.NODE_ENV || "development",
-    githubToken: process.env.GITHUB_TOKEN || ''
+    githubToken: process.env.GITHUB_TOKEN || '',
+    smtpUrl: process.env.SMTP_URL || '',
+    emailUser: process.env.EMAIL_USER || '',
+    emailPassword: process.env.EMAIL_PASSWORD || '',
 }
 
 export default config
