98beeca1dd6de235d84665f784961ad9b2967ab0
Christoffer Gylin <christoffer@christoffergylin.com>
2025-12-01 17:00:38 +0100

    Changed SMTP_URL to MAIL_HOST in env and added MAIL_HOST and MAIL_RECIPIENT

--- apps/backend/src/config/config.ts
+++ apps/backend/src/config/config.ts
@@ -6,18 +6,22 @@ type Config = {
     port: number;
     nodeEnv: string;
     githubToken: string;
-    smtpUrl: string;
+    emailHost: string;
+    emailPort: number;
     emailUser: string;
     emailPassword: string;
+    emailRecipient: string;
 }
 
 const config: Config = {
     port: Number(process.env.PORT) || 3000,
     nodeEnv: process.env.NODE_ENV || "development",
     githubToken: process.env.GITHUB_TOKEN || '',
-    smtpUrl: process.env.SMTP_URL || '',
+    emailHost: process.env.EMAIL_HOST || '',
+    emailPort: Number(process.env.EMAIL_PORT) || 465,
     emailUser: process.env.EMAIL_USER || '',
     emailPassword: process.env.EMAIL_PASSWORD || '',
+    emailRecipient: process.env.EMAIL_RECIPIENT || '',
 }
 
 export default config
