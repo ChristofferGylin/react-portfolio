ef88969d363988230fe5c8f11bceb3534c7feee3
Christoffer Gylin <christoffer@christoffergylin.com>
2025-12-10 17:27:34 +0100

    Made cors conditional depending on environment

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -2,10 +2,18 @@ import express from "express"
 import githubRoutes from "./routes/githubRoutes.ts"
 import emailRoutes from "./routes/emailRoutes.ts"
 import { errorHandler } from "./middleware/errorHandler.ts"
+import cors from "cors"
+import config from "./config/config.ts"
 
 const app = express()
 
 app.use(express.json())
+
+if (config.nodeEnv === "development")  {
+    app.use(cors({
+        origin: "http://localhost:5173"
+    }))
+}
 
 app.use("/api/github", githubRoutes)
 app.use("/api/email", emailRoutes)
