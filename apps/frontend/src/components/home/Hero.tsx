052346a45971ccf55f1d55f03773cfb9f130d2ad
Christoffer Gylin <christoffer@christoffergylin.com>
2025-12-11 16:17:35 +0100

    Implemented i18next in Hero component

--- apps/frontend/src/components/home/Hero.tsx
+++ apps/frontend/src/components/home/Hero.tsx
@@ -1,17 +1,17 @@
+import { useTranslation } from "react-i18next"
 import GithubWidget from "./GithubWidget"
 
 const Hero = () => {
 
+    const { t } = useTranslation(['home'])
+
     return (
         <section className="w-full grid grid-cols-2 gap-32 p-24">
             <div className="aspect-square flex flex-col gap-6 overflow-hidden">
-                <p>{content.greeting}</p>
+                <p>{t("intro.hi", { ns: "home"})}</p>
                 <h1>Christoffer Gylin</h1>
-                <p className="text-4xl font-bold">{content.tagline}</p>
-                <div className="flex flex-col gap-6 overflow-hidden text-xl">
-                    {<p>{bodyText[0]}</p>}
-                    {<p className="line-clamp-5">{bodyText[1]}..</p>}
-                </div>
+                <p className="text-4xl font-bold">{t("intro.tagline1", { ns: "home"})}</p>
+                <p className="text-4xl font-bold">{t("intro.tagline2", { ns: "home"})}</p>
             </div>
             <GithubWidget />
         </section>
