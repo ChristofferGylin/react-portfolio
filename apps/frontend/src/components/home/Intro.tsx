e331f9c1b3f08b9c7f0f19c42e7f9a127a4db9ce
Christoffer Gylin <christoffer@christoffergylin.com>
2025-12-11 19:33:13 +0100

    Extracted typing effect into seperate Typewriter component

--- apps/frontend/src/components/home/Intro.tsx
+++ apps/frontend/src/components/home/Intro.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useRef, useState } from "react"
 import { useTranslation } from "react-i18next"
+import Typewriter from "./Typewriter"
 
 const techs = [
     "JavaScript",
@@ -10,70 +10,16 @@ const techs = [
     "C++",
 ]
 
-const typingDelay = 150
-const waitingDelay = 1200
-
 const Intro = () => {
-
-    const [typed, setTyped] = useState("")
-    const wordIndex = useRef(0)
-    const letterIndex = useRef(0)
-    const isDeleting = useRef(false)
-    const timer = useRef<number>(0)
  
     const { t } = useTranslation(['home'])
-
-    useEffect(() => {
-
-        const typeText = () => {
-            if (isDeleting.current) {
-                setTyped((old) => {
-                    const endIndex = old.length - 1
-
-                    if (endIndex < 0) {
-                        isDeleting.current = false
-                        return ""
-                    }
-                    return old.slice(0, endIndex)
-                })
-
-                timer.current = setTimeout(typeText, typingDelay)
-            } else {
-
-                setTyped(techs[wordIndex.current].slice(0, letterIndex.current))
-
-                if (letterIndex.current >= techs[wordIndex.current].length) {
-                    
-                    if (wordIndex.current + 1 >= techs.length) {
-                        wordIndex.current = 0
-                    } else {
-                        wordIndex.current++
-                    }
-
-                    isDeleting.current = true
-                    letterIndex.current = 0
-                    timer.current = setTimeout(typeText, waitingDelay)
-
-
-                } else {
-                    letterIndex.current++
-                    timer.current = setTimeout(typeText, typingDelay)
-                }
-
-            }
-        }
-
-        typeText()
-        return () => clearTimeout(timer.current)
-        
-    }, [])
 
     return (
         <div className="aspect-square flex flex-col gap-6 overflow-hidden">
             <p>{t("intro.hi", { ns: "home"})}</p>
             <h1>Christoffer Gylin</h1>
             <p className="text-4xl font-bold">{t("intro.tagline1", { ns: "home"})}</p>
-            <p className="text-4xl font-bold">{t("intro.tagline2", { ns: "home"})} {typed}<span className="inline-block h-[80%]  w-0.5 bg-white ml-1 animate-blink" /></p>
+            <p className="text-4xl font-bold">{t("intro.tagline2", { ns: "home"})} <Typewriter words={techs} /></p>
         </div>
     )
 }
---
+++ apps/frontend/src/components/home/Typewriter.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useRef, useState } from "react"
+
+type TypewriterProps = {
+    typingDelay?: number;
+    waitingDelay?: number;
+    words: string[];
+}
+
+const Typewriter = ({typingDelay = 200, waitingDelay = 1200, words}: TypewriterProps) => {
+
+    const [typed, setTyped] = useState("")
+    const wordIndex = useRef(0)
+    const letterIndex = useRef(0)
+    const isDeleting = useRef(false)
+    const timer = useRef<number>(0)
+
+    useEffect(() => {
+
+        const typeText = () => {
+            if (isDeleting.current) {
+                setTyped((old) => {
+                    const endIndex = old.length - 1
+
+                    if (endIndex < 0) {
+                        isDeleting.current = false
+                        return ""
+                    }
+                    return old.slice(0, endIndex)
+                })
+
+                timer.current = setTimeout(typeText, typingDelay)
+            } else {
+
+                setTyped(words[wordIndex.current].slice(0, letterIndex.current))
+
+                if (letterIndex.current >= words[wordIndex.current].length) {
+                    
+                    if (wordIndex.current + 1 >= words.length) {
+                        wordIndex.current = 0
+                    } else {
+                        wordIndex.current++
+                    }
+
+                    isDeleting.current = true
+                    letterIndex.current = 0
+                    timer.current = setTimeout(typeText, waitingDelay)
+
+
+                } else {
+                    letterIndex.current++
+                    timer.current = setTimeout(typeText, typingDelay)
+                }
+
+            }
+        }
+
+        timer.current = setTimeout(typeText, typingDelay)
+        return () => clearTimeout(timer.current)
+        
+    }, [])
+
+    return <>{typed}<span className="inline-block h-[80%]  w-0.5 bg-white ml-1 animate-blink" /></>
+}
+
+export default Typewriter
