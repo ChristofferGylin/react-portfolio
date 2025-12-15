1c2913564c825640e48a9e67a4838c10fd8abbd7
Christoffer Gylin <christoffer@christoffergylin.com>
2025-12-15 20:11:58 +0100

    Adjusted font sizes for Intro component

--- apps/frontend/src/components/home/Intro.tsx
+++ apps/frontend/src/components/home/Intro.tsx
@@ -16,10 +16,10 @@ const Intro = () => {
 
     return (
         <div className="xl:relative flex justify-center w-full h-full">
-            <div className="lg:relative h-fit w-full lg:w-4/5 xl:w-3/4 flex flex-col gap-6 overflow-hidden xl:p-2 xl:top-12">
+            <div className="lg:relative h-fit w-full lg:w-4/5 xl:w-3/4 flex flex-col gap-6 md:gap-10 overflow-hidden px-2 xl:p-2 xl:top-12">
                 <p className="text-2xl sm:text-4xl">{t("intro.hi", { ns: "home"})}</p>
-                <h1 className="text-4xl sm:text-5xl md:text-7xl">Christoffer Gylin</h1>
-                <p className="text-2xl sm:text-4xl md:text-4xl font-bold">{t("intro.tagline", { ns: "home"})} <Typewriter words={techs} /></p>
+                <h1 className="text-5xl sm:text-6xl md:text-7xl">Christoffer Gylin</h1>
+                <p className="text-3xl sm:text-4xl md:text-5xl font-bold">{t("intro.tagline", { ns: "home"})} <Typewriter words={techs} /></p>
             </div>
         </div>
     )
