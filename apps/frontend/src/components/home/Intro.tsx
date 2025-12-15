fd11949fe673c98d6c82efca2df47808bbbe1ffe
Christoffer Gylin <christoffer@christoffergylin.com>
2025-12-15 22:03:20 +0100

    Adjusted alignment and max width for Intro component

--- apps/frontend/src/components/home/Intro.tsx
+++ apps/frontend/src/components/home/Intro.tsx
@@ -15,13 +15,13 @@ const Intro = () => {
     const { t } = useTranslation(['home'])
 
     return (
-        <div className="xl:relative flex justify-center w-148 2xl:w-200 h-full">
-            <div className="lg:relative h-fit w-full lg:w-4/5 xl:w-3/4 flex flex-col gap-6 md:gap-10 px-2 xl:p-2 xl:top-12">
-                <p className="text-3xl">{t("intro.hi", { ns: "home"})}</p>
+        
+            <div className="h-fit flex flex-col self-center justify-self-center max-w-100 sm:max-w-none sm:w-148 2xl:w-200 gap-6 md:gap-10 px-2 xl:p-2">
+                <p className="text-2xl sm:text-3xl">{t("intro.hi", { ns: "home"})}</p>
                 <h1 className="text-5xl sm:text-6xl md:text-7xl">Christoffer Gylin</h1>
-                <p className="text-3xl">{t("intro.tagline", { ns: "home"})} <Typewriter words={techs} /></p>
+                <p className="text-2xl sm:text-3xl">{t("intro.tagline", { ns: "home"})} <Typewriter words={techs} /></p>
             </div>
-        </div>
+        
     )
 }
 
