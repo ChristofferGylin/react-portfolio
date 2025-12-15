dd45e9bda4b9c9088dcbe3bf44ae188e26ff83cb
Christoffer Gylin <christoffer@christoffergylin.com>
2025-12-15 21:44:55 +0100

    Changed font sizes and set fixed withs for Intro component

--- apps/frontend/src/components/home/Intro.tsx
+++ apps/frontend/src/components/home/Intro.tsx
@@ -15,11 +15,11 @@ const Intro = () => {
     const { t } = useTranslation(['home'])
 
     return (
-        <div className="xl:relative flex justify-center w-full h-full">
-            <div className="lg:relative h-fit w-full lg:w-4/5 xl:w-3/4 flex flex-col gap-6 md:gap-10 overflow-hidden px-2 xl:p-2 xl:top-12">
-                <p className="text-2xl sm:text-4xl">{t("intro.hi", { ns: "home"})}</p>
+        <div className="xl:relative flex justify-center w-148 2xl:w-200 h-full">
+            <div className="lg:relative h-fit w-full lg:w-4/5 xl:w-3/4 flex flex-col gap-6 md:gap-10 px-2 xl:p-2 xl:top-12">
+                <p className="text-3xl">{t("intro.hi", { ns: "home"})}</p>
                 <h1 className="text-5xl sm:text-6xl md:text-7xl">Christoffer Gylin</h1>
-                <p className="text-3xl sm:text-4xl md:text-5xl font-bold">{t("intro.tagline", { ns: "home"})} <Typewriter words={techs} /></p>
+                <p className="text-3xl">{t("intro.tagline", { ns: "home"})} <Typewriter words={techs} /></p>
             </div>
         </div>
     )
