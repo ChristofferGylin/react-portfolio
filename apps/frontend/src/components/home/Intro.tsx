79c6976ce3337c7bd961c1ca6cddc4be2004a246
Christoffer Gylin <christoffer@christoffergylin.com>
2025-12-16 21:50:06 +0100

    Adjusted widths for Intro

--- apps/frontend/src/components/home/Intro.tsx
+++ apps/frontend/src/components/home/Intro.tsx
@@ -16,7 +16,7 @@ const Intro = () => {
 
     return (
         
-            <div className="h-fit flex flex-col lg:self-center justify-self-center max-w-100 sm:max-w-none sm:w-148 lg:w-120 2xl:w-148 3xl:w-200 gap-6 md:gap-10 px-2 xl:p-2">
+            <div className="h-fit flex flex-col lg:self-center justify-self-center max-w-100 sm:max-w-none sm:w-120 2xl:w-148 3xl:w-200 gap-6 md:gap-10 px-2 xl:p-2">
                 <p className="text-2xl lg:text-3xl">{t("intro.hi", { ns: "home"})}</p>
                 <h1 className="text-5xl lg:text-7xl">Christoffer Gylin</h1>
                 <p className="text-2xl lg:text-3xl">{t("intro.tagline", { ns: "home"})} <Typewriter words={techs} /></p>
