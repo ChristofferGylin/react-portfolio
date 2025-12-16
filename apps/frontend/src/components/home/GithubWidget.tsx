5785abb2cbf497fb1790efe451f63f3391e2d5b3
Christoffer Gylin <christoffer@christoffergylin.com>
2025-12-16 21:44:12 +0100

    Adjusted font sizes for GithubWidget

--- apps/frontend/src/components/home/GithubWidget.tsx
+++ apps/frontend/src/components/home/GithubWidget.tsx
@@ -66,8 +66,8 @@ const GithubWidget = () => {
 
     return (
         <div className="justify-self-center self-center w-full h-fit border border-cyan-300 foreground rounded-xl flex flex-col p-5 shadow shadow-cyan-200/30">
-            <h1 className="text-2xl md:text-3xl text-cyan-300 border-b border-cyan-200/50 p-3">{t("github.heading", {ns: "home"})}</h1>
-            <ul className="w-full flex flex-col gap-3 p-4 text-lg md:text-2xl text-cyan-400">
+            <h1 className="text-2xl xl:text-3xl text-cyan-300 border-b border-cyan-200/50 p-3">{t("github.heading", {ns: "home"})}</h1>
+            <ul className="w-full flex flex-col gap-3 p-4 text-lg xl:text-2xl text-cyan-400">
                 {Object.entries(languages).map(([key, lang]) => {
                 return (
                     <li className="flex flex-col gap-1 w-full" key={key}>
