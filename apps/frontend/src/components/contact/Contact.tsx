3d9d6c87ad7e3d87029538efe56934226012cb8b
Christoffer Gylin <christoffer@christoffergylin.com>
2025-12-18 22:50:48 +0100

    Implemented i18n translations

--- apps/frontend/src/components/contact/Contact.tsx
+++ apps/frontend/src/components/contact/Contact.tsx
@@ -1,7 +1,13 @@
+import { useTranslation } from "react-i18next"
+
 const Contact = () => {
+    
+    const { t } = useTranslation("contact")
+    
     return (
         <div>
-            <h1>Contact</h1>
+            <p>{t("tagline", { ns: "contact" })}</p>
+            <h1>{t("heading", { ns: "contact" })}</h1>
         </div>
     )
 }
