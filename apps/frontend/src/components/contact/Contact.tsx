9a33dfee1f878962c32a9390527aec784c5b1a20
Christoffer Gylin <christoffer@christoffergylin.com>
2026-01-05 20:32:20 +0100

    Fixed layout and styling

--- apps/frontend/src/components/contact/Contact.tsx
+++ apps/frontend/src/components/contact/Contact.tsx
@@ -17,23 +17,50 @@ const Contact = () => {
 
     const { t } = useTranslation("contact")
     
-    return (
-        <div>
-            <p>{t("tagline", { ns: "contact" })}</p>
-            <h1>{t("heading", { ns: "contact" })}</h1>
-            <form onSubmit={handleSubmit((data) => {
-                console.log(data)
-            })}>
-                <input {...register("message", { required: t("text_error_msg", { ns: "contact" })})} placeholder={t("text_placeholder", { ns: "contact"})} />
-                <p>{errors.message?.message}</p>
-                <input {...register("name", { required: t("name_error_msg", { ns: "contact" })})} placeholder={t("name_placeholder", { ns: "contact"})} />
-                <p>{errors.name?.message}</p>
-                <input {...register("email", { required: t("email_error_msg", { ns: "contact" })})} placeholder={t("email_placeholder", { ns: "contact"})} />
-                <p>{errors.email?.message}</p>
-                <input className="cursor-pointer" type="submit" value={t("button", { ns: "contact"})} />
+return (
+    <div className="w-full flex justify-center min-h-[calc(100vh-8rem)]">
+        <div className="flex flex-col w-full lg:w-3/4 min-h-0 p-10 lg:px-0">
+            <div className="flex flex-col gap-2 mb-8">
+                <p className="text-2xl">{t("tagline", { ns: "contact" })}</p>
+                <h1 className="text-5xl">{t("heading", { ns: "contact" })}</h1>
+            </div>
+            <form
+                className="flex flex-col flex-1 min-h-0 w-full"
+                onSubmit={handleSubmit((data) => console.log(data))}
+            >
+                <div className="flex flex-col flex-1 min-h-0 mb-4">
+                    <textarea
+                        className="text-input flex-1 min-h-0"
+                        {...register("message", { required: t("text_error_msg", { ns: "contact" }) })}
+                        placeholder={t("text_placeholder", { ns: "contact" })}
+                    />
+                    <p className="input-warning">{errors.message?.message}</p>
+                </div>
+                <div className="grid gap-1">
+                    <input
+                        className="text-input"
+                        {...register("name", { required: t("name_error_msg", { ns: "contact" }) })}
+                        placeholder={t("name_placeholder", { ns: "contact" })}
+                    />
+                    <p className="input-warning">{errors.name?.message}</p>
+
+                    <input
+                        className="text-input"
+                        {...register("email", { required: t("email_error_msg", { ns: "contact" }) })}
+                        placeholder={t("email_placeholder", { ns: "contact" })}
+                    />
+                    <p className="input-warning">{errors.email?.message}</p>
+                </div>
+                <input
+                    className="cursor-pointer h-12 mt-4"
+                    type="submit"
+                    value={t("button", { ns: "contact" })}
+                />
             </form>
         </div>
-    )
+  </div>
+);
+
 }
 
 export default Contact
