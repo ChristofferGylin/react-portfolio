ea03c42f36ac398e1f1a82e589a38cbb16589d85
Christoffer Gylin <christoffer@christoffergylin.com>
2026-01-06 19:18:49 +0100

    Created apiClient for sending message

---
+++ apps/frontend/src/types/api.ts
@@ -0,0 +1,5 @@
+export type EmailParams = {
+    name: string;
+    email: string;
+    message: string;
+}
--- apps/frontend/src/utils/apiClient.ts
+++ apps/frontend/src/utils/apiClient.ts
@@ -1,7 +1,31 @@
+import { type EmailParams } from "../types/api"
+
 export const resolveApiPrefix = () => {
     if (import.meta.env.PROD) {
         return ''
     } else {
         return 'http://localhost:3000'
     }
+}
+
+export const sendMessage = async (params: EmailParams) => {
+    try {
+        const response = await fetch(`${resolveApiPrefix()}/api/email`, {
+            method: "POST",
+            body: JSON.stringify(params),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+
+        if (!response.ok) {
+            throw new Error(`Status: ${response.status}`)
+        }
+
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error(error)
+    }
+    
 }
