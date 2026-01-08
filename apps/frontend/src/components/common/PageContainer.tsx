9355b12c4a683bb4424abd233a08b0e9acbcf109
Christoffer Gylin <christoffer@christoffergylin.com>
2026-01-08 21:52:42 +0100

    Chnaged PageContainer to include outer container

--- apps/frontend/src/components/common/PageContainer.tsx
+++ apps/frontend/src/components/common/PageContainer.tsx
@@ -1,9 +1,11 @@
 import type { ReactNode } from "react"
 
-const PageContainer = ({children}:{ children: ReactNode}) => {
+const PageContainer = ({children, heightScreen}:{ children: ReactNode, heightScreen?: boolean}) => {
     return (
-        <div className="flex flex-col w-full lg:w-3/4 min-h-0 p-10 lg:px-0">
-            {children}
+        <div className={`w-full flex justify-center ${heightScreen ? "min-h-[calc(100vh-8rem)]" : ""}`}>
+            <div className="flex flex-col w-full lg:w-3/4 min-h-0 p-10 lg:px-0">
+                {children}
+            </div>
         </div>
     )
 }
