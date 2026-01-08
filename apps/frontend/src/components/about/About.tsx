0867b65810ee67befcce0a00332e0fdfd568e22f
Christoffer Gylin <christoffer@christoffergylin.com>
2026-01-08 22:03:34 +0100

    Added translations from i18next

--- apps/frontend/src/components/about/About.tsx
+++ apps/frontend/src/components/about/About.tsx
@@ -1,9 +1,19 @@
+import { useTranslation } from "react-i18next"
 import PageContainer from "../common/PageContainer"
 
 const About = () => {
+
+    const { t } = useTranslation("about")
+
+    const paragraphs = t("paragraphs", { ns: "about", returnObjects: true}) as string[]
     return (
         <PageContainer>
-            <h1>About</h1>
+            <h1>{t("heading1", { ns: "about" })}</h1>
+            <div>
+                {paragraphs.map((para, index) => (
+                    <p key={`aboutParagraph${index}`}>{para}</p>
+                ))}
+            </div>
         </PageContainer>
     )
 }
