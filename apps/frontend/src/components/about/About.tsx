9b8b30a677ceee94e1ddc2c4d3b1b6f356c98b7d
Christoffer Gylin <christoffer@christoffergylin.com>
2026-01-08 22:09:07 +0100

    Changed text sizes, margins and gaps

--- apps/frontend/src/components/about/About.tsx
+++ apps/frontend/src/components/about/About.tsx
@@ -8,8 +8,8 @@ const About = () => {
     const paragraphs = t("paragraphs", { ns: "about", returnObjects: true}) as string[]
     return (
         <PageContainer>
-            <h1>{t("heading1", { ns: "about" })}</h1>
-            <div>
+            <h1 className="text-5xl mb-8">{t("heading1", { ns: "about" })}</h1>
+            <div className="flex flex-col gap-6 text-xl">
                 {paragraphs.map((para, index) => (
                     <p key={`aboutParagraph${index}`}>{para}</p>
                 ))}
