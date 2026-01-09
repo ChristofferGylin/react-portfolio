d714b69a81f7453ec6048ca86b3d8f522b4dfc18
Christoffer Gylin <christoffer@christoffergylin.com>
2026-01-09 18:14:36 +0100

    Added main tag to App.tsx with margin top 14 to account for menu

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,27 +1,29 @@
-import { Suspense } from "react"
-import Menu from "./components/menu/Menu"
-import { Route, Routes } from "react-router-dom"
-import Home from "./components/home/Home"
-import About from "./components/about/About"
-import Contact from "./components/contact/Contact"
-import CV from "./components/cv/CV"
-import Portfolio from "./components/portfolio/Portfolio"
-import NotFound from "./components/notFound/Not Found"
+import { Suspense } from "react";
+import Menu from "./components/menu/Menu";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import CV from "./components/cv/CV";
+import Portfolio from "./components/portfolio/Portfolio";
+import NotFound from "./components/notFound/Not Found";
 
 function App() {
   return (
     <>
       <Menu />
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/cv" element={<CV />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <main className="mt-14">
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cv" element={<CV />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
     </>
-  )
+  );
 }
 
 export default function WrappedApp() {
@@ -29,5 +31,5 @@ export default function WrappedApp() {
     <Suspense fallback="Loading...">
       <App />
     </Suspense>
-  )
+  );
 }
