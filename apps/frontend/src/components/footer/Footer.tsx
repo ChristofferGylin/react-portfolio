c0d4e8af7163daff1aa1b5a26b44b5e10d3d1480
Christoffer Gylin <christoffer@christoffergylin.com>
2026-01-14 21:20:55 +0100

    Created SvgLink component

--- apps/frontend/src/components/footer/Footer.tsx
+++ apps/frontend/src/components/footer/Footer.tsx
@@ -1,5 +1,20 @@
+import { linkedInPath } from "../../assets/icons/svgPaths";
+import { sky200, sky50 } from "../../assets/tailwindHexColors";
+import SvgLink from "../UI/SvgLink";
+
 const Footer = () => {
-  return <footer className="header-footer bottom-0 foreground">Footer</footer>;
+  return (
+    <footer className="header-footer bottom-0 foreground">
+      <nav>
+        <SvgLink
+          path={linkedInPath}
+          color={sky200}
+          hoverColor={sky50}
+          href="https://linkedin.com"
+        />
+      </nav>
+    </footer>
+  );
 };
 
 export default Footer;
